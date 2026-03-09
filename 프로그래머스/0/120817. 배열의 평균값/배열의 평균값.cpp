9c6ee9b671a37b901bf069914c3eb675666f7bdc
Mygarden797 <75717176+Mygarden797@users.noreply.github.com>
2026-03-09 09:17:23 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> numbers) {
+    double answer = 0;
+    float sum = 0;
+    for(int i: numbers)
+    {
+        sum+=i;
+    }
+    answer = sum/numbers.size();
+    return answer;
+}
